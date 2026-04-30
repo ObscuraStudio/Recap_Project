90057642bb79bd5bae55ba57afd5493733122017
Cathulhu <maximilian.tofelde@gmail.com>
2026-04-30 12:28:20 +0200

    implemented containsDigit Method and corresponding Tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,5 +10,17 @@ public class Main {
         }
         return password.length() >= min;
     }
+
+    public static boolean containsDigit(String password) {
+        if (password == null) {
+            return false;
+        }
+        for (char c : password.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -29,4 +29,31 @@ class MainTest {
     public void hasMinLength_stringIsEmpty_shouldReturnFalse() {
         assertFalse(Main.hasMinLength("", 8));
     }
+
+    // Test cases for containsDigit method
+
+    @Test
+    public void containsDigit_RejectsArabicDigit_shouldReturnFalse() {
+        assertFalse(Main.containsDigit("password٥"));
+    }
+
+    @Test
+    public void containsDigit_RejectsFullwidthDigit_shouldReturnFalse() {
+        assertFalse(Main.containsDigit("password５"));
+    }
+
+    @Test
+    public void containsDigit_AcceptsAsciiDigit_shouldReturnTrue() {
+        assertTrue(Main.containsDigit("password5"));
+    }
+
+    @Test
+    public void containsDigit_NullSafe_shouldReturnFalse() {
+        assertFalse(Main.containsDigit(null));
+    }
+
+    @Test
+    public void containsDigit_noDigit_shouldReturnFalse() {
+        assertFalse(Main.containsDigit("password"));
+    }
 }
